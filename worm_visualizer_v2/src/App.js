d958daf608d514bb1197be6f8b4ebcfa20b68716
Exr0n <mail@exr0n.com>
2022-03-13 19:01:30 -0700

    visualization app SR

---
+++ worm_visualizer_v2/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.png';
+
+function App() {
+    return (
+        <div className="w-screen h-screen bg-gray-700">
+        </div>
+    );
+}
+
+export default App;
