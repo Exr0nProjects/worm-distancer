d4c0eb420697ba10315c08bbe9abff396408a321
Exr0n <mail@exr0n.com>
2022-03-13 21:19:10 -0700

    need to think more about the reducer

---
+++ worm_visualizer_v2/src/reducer.js
@@ -0,0 +1,12 @@
+import update from "react-addons-update";
+
+export const INITAL_STATE = {
+    datas: []
+}
+
+export function reducer_fn(state, { action, payload }) {
+    switch (action) {
+        case 'change_frame':
+
+    }
+}
