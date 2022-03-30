3a810167af9ccf441f848b31e408cd0968bb9cab
Exr0n <mail@exr0n.com>
2022-03-30 01:54:33 -0700

    copy dataframe instead of overwriting by ref

--- process/compare.py
+++ process/compare.py
@@ -65,10 +65,20 @@ def jankily_read_combined_data(filename):
 
     return ret
 
-def jankily_stddev(dfds, blacklist):
+def jankly_show_data_distribution(dfds, row, col,
+        title=None, xlabel=None, ylabel=None, strain=None):
+    fig, ax = plt.subplots()
+    ax.hist([dfd['df'][row][col] for dfd in dfds], color=COLOR_DEFAULT)
+
+    ax.set_xlabel(xlabel or row)
+    ax.set_ylabel(ylabel or "# of annotators")
+    ax.set_title(title or f"Distribution of {row} at time {col}{' in ' + strain if strain else ''}")
+    plt.show()
+
+def jankily_stddev(dfds, blacklist=[]):
     """Takes the cellwise stdev of a list of CSVs."""
     dfs = [dfd['df'] for dfd in dfds]
-    out = dfs[0]  # clone dfs cause im lazyy
+    out = dfs[0].copy() # OPTM copy expensive
 
     for name in dfs[0].columns:
         if name in blacklist:
@@ -84,15 +94,32 @@ def jankily_stddev(dfds, blacklist):
                 )
     return out
 
-def jankly_show_data_distribution(dfds, row, col,
-        title=None, xlabel=None, ylabel=None, strain=None):
-    fig, ax = plt.subplots()
-    ax.hist([dfd['df'][row][col] for dfd in dfds], color=COLOR_DEFAULT)
+def jankily_mean(dfds):
+    dfs = [dfd['df'] for dfd in dfds]
+    out = dfs[0].copy() # OPTM
 
-    ax.set_xlabel(xlabel or row)
-    ax.set_ylabel(ylabel or "# of annotators")
-    ax.set_title(title or f"Distribution of {row} at time {col}{' in ' + strain if strain else ''}")
-    plt.show()
+    for name in dfs[0].columns:
+        for i, _x in enumerate(dfs[0][name]):
+            points = [df[name][i] for df in dfs]
+            if math.isnan(set(points).pop()):
+                out[name][i] = float('nan')
+            else:
+                out[name][i] = stats.fmean(
+                    list(filter(lambda x: not math.isnan(x), points))
+                )
+    return out
+
+
+def jankily_make_line_plot(dfdss, colors, col, title=None, xlabel=None, ylabel=None):
+    fig, ax = plt.subplots()
+    print(dfdss[0])
+    means = [jankily_mean(dfds) for dfds in dfdss]
+    stddevs = [jankily_stddev(dfds) for dfds in dfdss]
+
+    for mean, stdd in zip(means, stddevs):
+        mean = mean[col]
+        stdd = stdd[col]
+        print(mean, stdd)
 
 from matplotlib.patches import Ellipse
 def plot_bounding_ellipse(ax, points, label, tolerance=1e-4):
@@ -123,21 +150,6 @@ def plot_all_2d(filenames):
         plot_skeleton_2d(ax, df, name)
     plt.show()
 
-
-# def plot_3d_by_labeller(filenames):
-#     dfs = [(name, pd.read_csv(name, sep='	')) for name in filenames]
-#     # 3d plotting from https://jakevdp.github.io/PythonDataScienceHandbook/04.12-three-dimensional-plotting.html
-#     ax = plt.axes(projection='3d')
-#     cmaps = ['Blues', 'Greens', 'Reds']*2
-#     for (labeller, df), cmap in zip(dfs, cmaps):
-#         # print(labeller, '\n', df)
-#         for t, row in df.iterrows():
-#             ydata = row[4::2]
-#             zdata = row[5::2]
-#             xdata = [t] * len(ydata)
-#             print(labeller, t, f'(color={cmap}) (lens)', len(ydata), len(zdata))
-#             ax.scatter3D(xdata, ydata, zdata, s=500, c=zdata, cmap=cmap)
-#     plt.show()
 
 def plot_3d_by_point(dfds):
     # dfs = [(name, pd.read_csv(name, sep='	')) for name in filenames]
@@ -256,15 +268,19 @@ VIDEOS_BY_STRAIN = {
 def dfd_filter(datas, author=None, notauthor=None, strain=None, worm=None):
     # assumptions: every video contains worms of one strain, every worm is uniquely identified by video name and beginning time stamp
     def pred(dfd):
+        # print("checking", dfd['author'], dfd['video'], dfd['time'])
         if author is not None and dfd['author'] != author:
+            # print("    author whitelist failed", dfd['author'])
             return False
         if notauthor is not None and dfd['author'] in notauthor:
+            # print("    author blacklist triggered", dfd['author'])
             return False
         if strain is not None and dfd['video'] not in VIDEOS_BY_STRAIN[strain]:
             return False
         if worm is not None and f"{dfd['video']}:{str(dfd['time']) }" != worm:
-            # print(dfd['video'], dfd['time'])
+            # print('    worm failed', dfd['author'], dfd['video'], dfd['time'])
             return False
+        # print(' => returning true for dfd', dfd['author'], dfd['video'], dfd['time'])
         return True
     return [x for x in datas if pred(x)]
 
@@ -284,13 +300,21 @@ if __name__ == '__main__':
     with open('all_data_procced.pickle', 'rb') as rf:
         datas = pickle.load(rf)
 
-    # datas = dfd_filter(datas, strain='n2')
-    datas = dfd_filter(datas, notauthor=['zander'], worm='LA.ALS.3.25.22.#1.mp4:90')
-    print([(dfd['author'], dfd['df']) for dfd in datas])
-    jankly_show_data_distribution(datas, 'sum_angles', 2)
+    # POSTER TODO: realign headings; how to collate multiple worms of the same strain
 
-    data = jankily_stddev(datas, [])
-    print(data)
+    # for dfd in datas:
+    #     print(dfd['author'], dfd['video'], dfd['time'])
+
+    # datas = dfd_filter(datas, strain='n2')
+    datas_n2 = dfd_filter(datas, notauthor=['zander'], worm='LA.ALS.3.25.22.#1.mp4:90')
+    datas_n2oil = dfd_filter(datas, notauthor=['zander'], worm='AL.ALS.3.25.22.#6.mp4:56')
+    datas_cboil = dfd_filter(datas, notauthor=['zander'], worm='LA.ALS.3.25.22.#7.mp4:20')
+    print(len(datas_n2), len(datas_n2oil), len(datas_cboil))
+    # print([(dfd['author'], dfd['df']) for dfd in datas_n2])
+    # jankly_show_data_distribution(datas_n2, 'sum_angles', 2)
+
+    jankily_make_line_plot([datas_n2, datas_n2oil, datas_cboil],
+            [COLOR_N2, COLOR_AM, COLOR_CB], 'ellipse_ecentricity')
 
     # plot_all_2d(filenames)
 
